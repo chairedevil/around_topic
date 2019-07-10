915712a5f8661f23457213915d18eaee0450a144
Kensuke Narita <laughingman2525@gmail.com>
2019-07-10 16:47:07 +0900

    timeline_update

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,14 +3,33 @@
         <h1>TIMELINE</h1>
         <p>現在位置情報：{{ nowGeo }}</p>
         <p>検索位置情報：{{ searchbarGeo }}</p> 
+        <template v-if="lat && lng">
+             <apptwitter
+                v-bind:twitterLat="lat"
+                v-bind:twitterLng="lng"
+                @updateTweetResult = "tweetResults = $event"
+                >
+            </apptwitter> 
+        </template>
     </div>    
 </template>
 <script>
 import { mapState } from 'vuex'
 import { VueMasonryPlugin } from 'vue-masonry'
+import myTwitter from './mapComponents/Twitter.vue'
 
 export default {
     name: 'Timeline',
+    components: {
+        apptwitter: myTwitter
+    },
+    data() {
+        return {
+            lat: null,
+            lng: null,
+            tweetResults: null,
+        }
+    },
     computed: {
         /*nowGeo: function(){
             return this.$store.state.nowGeo
@@ -20,13 +39,31 @@ export default {
         }*/
         ...mapState(['nowGeo', 'searchbarGeo'])
     },
+    methods: {
+        geoCheck(){
+            if(this.searchbarGeo){
+                console.log(this.searchbarGeo.lat);
+                this.lat = this.searchbarGeo.lat.toString();
+                this.lng = this.searchbarGeo.lng.toString();
+            }
+            else{
+                this.lat = this.nowGeo.lat.toString();
+                this.lng = this.nowGeo.lng.toString();
+            }
+        },
+    },
     watch: {
         nowGeo(newVal, oldVal) {
             console.log('nowGeo was change')
+            this.geoCheck()
         },
         searchbarGeo(newVal, oldVal) {
             console.log('searchbarGeo was change')
+            this.geoCheck()
         },
+        tweetResults(newVal, oldVal){
+            console.log(newVal);
+        }
     }
 }
 </script>
